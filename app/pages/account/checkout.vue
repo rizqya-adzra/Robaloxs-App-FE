e595adc1c7c83f49305d93aa0d5731cda9c1f780
rizqya-adzra <rizqyaadzrazahirasudrajat@smkwikrama.sch.id>
2026-01-15 20:21:37 +0700

    v1.0.11 feat: added checkout page

---
+++ app/pages/account/checkout.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="mx-auto max-w-[1050px] py-20">
+    <p class="font-inter font-bold text-4xl mb-5 text-center lg:text-start"><span class="text-primary">Checkout Produk</span> yang Kamu mau</p>
+    <div class="flex justify-center items-start">
+      <div class="flex flex-col lg:flex-row justify-between items-start gap-5">
+        <div class="space-y-5">
+          <CardsDefault step="1">
+            <template #title>
+              Masukan Username & Password Roblox
+            </template>
+            <template #inputs>
+              <div class="flex justify-start items-center gap-4">
+                <InputsDefault placeholder="username" variant="default" color="black" />
+                <InputsDefault placeholder="password" variant="default" color="black" />
+              </div>
+            </template>
+            <template #description>
+              Pastikan Username dan Password in game anda sudah sesuai dengan yang dicantumkan agar tidak terjadi kegagalan atau error di saat transaksi.
+            </template>
+          </CardsDefault>
+          <CardsDefault step="2">
+            <template #title>
+              Pilih Metode Pembayaran
+            </template>
+            <template #inputs>
+              <div class="space-y-5">
+                <div class="space-y-3">
+                  <p class="font-inter font-bold">QRIS & E-Wallet</p>
+                  <div class="border-2 border/50 w-[30%] rounded-full"></div>
+                  <div class="flex justify-start items-center gap-4">  
+                    <CheckboxesDefault :image="qris" modelValue="" value="100" qty="QRIS" price="Tanpa biaya tambahan" />        
+                    <CheckboxesDefault :image="gopay" modelValue="" value="100" qty="Gopay" price="Tanpa biaya tambahan" />              
+                  </div>
+                </div>
+                <div class="space-y-3">
+                  <p class="font-inter font-bold">Bank Transfer</p>
+                  <div class="border-2 border/50 w-[30%] rounded-full"></div>
+                  <div class="flex justify-start items-center gap-4">  
+                    <CheckboxesDefault :image="bca" modelValue="" value="100" qty="BCA" price="Tanpa biaya tambahan" />        
+                    <CheckboxesDefault :image="bni" modelValue="" value="100" qty="BNI" price="Tanpa biaya tambahan" />             
+                  </div>
+                  <div class="flex justify-start items-center gap-4">  
+                    <CheckboxesDefault :image="mandiri" modelValue="" value="100" qty="Mandiri" price="Tanpa biaya tambahan" />        
+                    <CheckboxesDefault :image="cimb_niaga" modelValue="" value="100" qty="CIMB Niaga" price="Tanpa biaya tambahan" />              
+                  </div>
+                  <div class="flex justify-start items-center gap-4">  
+                    <CheckboxesDefault :image="bri" modelValue="" value="100" qty="BRI" price="Tanpa biaya tambahan" />                  
+                  </div>
+                </div>
+              </div>
+            </template>
+          </CardsDefault>
+          <CardsDefault step="3">
+            <template #title>
+              Masukan Nomor Whatsapp & Email
+            </template>
+            <template #inputs>
+              <div class="flex justify-start items-center gap-4">
+                <InputsDefault placeholder="cth: 08xxxxxxx" variant="default" color="black" />
+                <InputsDefault placeholder="email cth: robaloxs@gmail.com" variant="default" color="black" />
+              </div>
+            </template>
+            <template #description>
+              No Whatsapp wajib di cantumkan agar admin bisa menghubungi untuk konfirmasi pesanan atau jika ada kendala.
+            </template>
+          </CardsDefault>
+        </div>
+        <CardsCheckout>
+          <template #item-name></template>
+          <template #payment-method></template>
+          <template #contact-info></template>
+          <template #total-price></template>
+        </CardsCheckout>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import robux from "~/assets/images/robux.png"
+import bca from "~/assets/images/payment/bca.png"
+import bni from "~/assets/images/payment/bni.png"
+import bri from "~/assets/images/payment/bri.png"
+import cimb_niaga from "~/assets/images/payment/cimb_niaga.png"
+import gopay from "~/assets/images/payment/gopay.png"
+import mandiri from "~/assets/images/payment/mandiri.png"
+import qris from "~/assets/images/payment/qris.png"
+</script>
